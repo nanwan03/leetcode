6843d541306eebf38d438b62ed30860f4a3ffe98
nanwan03 <wn1842@gmail.com>
2015-05-28 19:22:49 -0400

    commit

---
+++ Algorithm/Contains Duplicate II.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], i);
+            } else {
+                if (i - map.get(nums[i]) <= k) {
+                    return true;
+                } else {
+                    map.put(nums[i], i);
+                }
+            }
+        }
+        return false;
+    }
+}
