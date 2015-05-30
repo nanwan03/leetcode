fcba11ab9a9f48b61c48f690d3c652c07834cfe9
nanwan03 <wn1842@gmail.com>
2015-05-30 11:01:08 -0400

    commit

--- Algorithm/Maximum Gap.java
+++ Algorithm/Maximum Gap.java
@@ -3,20 +3,23 @@ public class Solution {
         if (num == null || num.length < 2) {
             return 0;
         }
-        int n = num.length;
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
         for (int i : num) {
             min = Math.min(i, min);
             max = Math.max(i, max);
         }
+        if (max == min) {
+            return 0;
+        }
+        int n = num.length;
         double dist = (double)(max - min) / (double)(n - 1);
         int[] minL = new int[n - 1];
         int[] maxL = new int[n - 1];
         Arrays.fill(minL, -1);
         Arrays.fill(maxL, -1);
         for (int i : num) {
-            int idx = (i == max) ? (n - 2) : ((int)((i - min) / dist));
+            int idx = (i == max) ? (n - 2) : (int)((i - min) / dist);
             if (minL[idx] == -1 || i < minL[idx]) {
                 minL[idx] = i;
             }
