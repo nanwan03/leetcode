a67965a273506d25382f37f09d51e93c591b108b
nanwan03 <wn1842@gmail.com>
2015-06-10 20:27:18 -0400

    commit

--- Algorithm/Implement Stack using Queues.java
+++ Algorithm/Implement Stack using Queues.java
@@ -1,29 +1,45 @@
 class Stack {
-    private Queue<Integer> queue = new ArrayDeque<>();
-    private Integer topElement = null; 
-    
-    // Push element x onto stack.
-    public void push(int x) {
-        topElement = x;
-        queue.offer(x);
-    }
-    
-    // Removes the element on top of the stack.
-    public void pop() {
-        for (int i = 0, size = queue.size() - 1; i < size; i++) {
-            queue.offer(queue.poll());
-        }
-    
-        queue.poll();
-    }
-    
-    // Get the top element.
-    public int top() {
-        return topElement;
-    }
-    
-    // Return whether the stack is empty.
-    public boolean empty() {
-        return queue.isEmpty();
-    }
+	Queue<Integer> queue1 = new LinkedList<Integer>();
+	Queue<Integer> queue2 = new LinkedList<Integer>();
+	List<Queue<Integer>> list = new ArrayList<Queue<Integer>>();
+	Stack() {
+		list.add(queue1);
+		list.add(queue2);
+	}
+	// Push element x onto stack.
+	public void push(int x) {
+	    if (!list.get(0).isEmpty()) {
+	    	list.get(0).offer(x);
+	    } else {
+	    	list.get(1).offer(x);
+	    }
+	}
+
+	// Removes the element on top of the stack.
+	public void pop() {
+	    int cur = list.get(0).isEmpty() ? 1 : 0;
+	    int alter = 1 - cur;
+	    while (list.get(cur).size() > 1) {
+	    	list.get(alter).add(list.get(cur).poll());
+	    }
+	    list.get(cur).poll();
+	}
+
+	// Get the top element.
+	public int top() {
+	    int ret = 0;
+	    int cur = list.get(0).isEmpty() ? 1 : 0;
+	    int alter = 1 - cur;
+	    while (list.get(cur).size() > 1) {
+	    	list.get(alter).add(list.get(cur).poll());
+	    }
+	    ret = list.get(cur).poll();
+	    list.get(alter).offer(ret);
+	    return ret;
+	}
+
+	// Return whether the stack is empty.
+	public boolean empty() {
+	    return queue1.isEmpty() && queue2.isEmpty();
+	}
 }
