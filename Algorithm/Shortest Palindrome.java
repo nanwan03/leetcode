38711567a59404882873e9daa637c64edcc833ba
nanwan03 <wn1842@gmail.com>
2015-06-13 16:11:26 -0400

    commit

--- Algorithm/Shortest Palindrome.java
+++ Algorithm/Shortest Palindrome.java
@@ -1,17 +1,24 @@
 public class Solution {
     public String shortestPalindrome(String s) {
-        String s_reverse = new StringBuilder(s).reverse().toString();
-        String str = s + "." + s_reverse;
-        int[] size = new int[str.length()];
-        for(int i = 1; i < str.length(); i++){
-            int temp = size[i - 1];
-            while(temp != 0 && str.charAt(temp) != str.charAt(i))
-                temp = size[temp - 1];
-            if(str.charAt(temp) == str.charAt(i))
-                temp++;
-            size[i] = temp;
+        String result = "";
+        if(s.length() == 0)
+            return result;
+        int[] next = new int[s.length() * 2 + 1];
+        next[0] = -1;
+        String mirror = s + new StringBuilder(s).reverse().toString();
+        int j = 0;
+        int k = -1;
+        while (j < s.length() * 2) {
+            if (k == -1 || mirror.charAt(j) == mirror.charAt(k)) {
+                j++;
+                k++;
+                next[j] = k;
+            } else {
+                k = next[k];
+            }
         }
-
-        return s_reverse.substring(0, s.length() - size[str.length() - 1]) + s;
+        int count = s.length() - next[s.length() * 2];
+        result = new StringBuilder(s.substring(s.length()-count, s.length())).reverse().toString() + s;
+        return result;
     }
 }
