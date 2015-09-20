4ae04b0071bcf66f07f63491e15ccbdcee10db0b
nanwan03 <wn1842@gmail.com>
2015-09-20 10:10:50 -0400

    commit

---
+++ Algorithm/Peeking Iterator.java
@@ -0,0 +1,42 @@
+// Java Iterator interface reference:
+// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
+class PeekingIterator implements Iterator<Integer> {
+    private final Iterator<Integer> iterator;
+    private boolean hasPeeked;
+    private Integer peekedElement;
+    
+	public PeekingIterator(Iterator<Integer> iterator) {
+	    // initialize any member here.
+	    if(iterator==null)
+            throw new NullPointerException();
+        else
+            this.iterator = iterator;
+	    
+	}
+
+    // Returns the next element in the iteration without advancing the iterator.
+	public Integer peek() {
+        if (!hasPeeked) {
+            peekedElement = iterator.next();
+            hasPeeked = true;
+        }
+        return peekedElement;
+	}
+
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	@Override
+	public Integer next() {
+	    if (!hasPeeked) 
+            return iterator.next();
+        Integer result = peekedElement;
+        hasPeeked = false;
+        peekedElement = null;
+        return result;
+	}
+
+	@Override
+	public boolean hasNext() {
+	    return hasPeeked || iterator.hasNext();
+	}
+}
