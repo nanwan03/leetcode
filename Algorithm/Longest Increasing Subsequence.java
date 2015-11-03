22612f661937ed74fa849b97b09c596a5bc75f57
nanwan03 <wn1842@gmail.com>
2015-11-02 20:40:06 -0500

    commit

---
+++ Algorithm/Longest Increasing Subsequence.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        int longest = 1;
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < nums.length; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    longest = Math.max(longest, dp[i]);
+                }
+            }
+        }
+        return longest;
+    }
+}
