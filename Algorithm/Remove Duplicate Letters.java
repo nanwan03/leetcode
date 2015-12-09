8bde840bd25693d77be7c80ff5278fc4ecb639f0
nanwan03 <wn1842@gmail.com>
2015-12-09 18:19:54 -0500

    commit

--- Algorithm/Remove Duplicate Letters.java
+++ Algorithm/Remove Duplicate Letters.java
@@ -1,19 +1,28 @@
 public class Solution {
     public String removeDuplicateLetters(String s) {
-        int[] cnt = new int[26];
-        boolean[] isCandidate = new boolean[26];
-        for (int i = 0; i < s.length(); i++) cnt[s.charAt(i) - 'a']++;
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            isCandidate[ch - 'a'] = true;
-            if (--cnt[ch - 'a'] == 0) break;
+        if (s == null || s.length() == 0) {
+            return s;
         }
-        for (int i = 0; i < 26; i++)
-            if (isCandidate[i]) {
-                char ch = (char) (i + 'a');
-                for (int j = 0; j < s.length(); j++)
-                    if (s.charAt(j) == ch) return ch + removeDuplicateLetters(s.substring(j + 1).replaceAll("" + ch, ""));
+        int[] counts = new int[256];
+        boolean[] isVisited = new boolean[256];
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            counts[c]++;
+        }
+        for (char c : s.toCharArray()) {
+            counts[c]--;
+            if (isVisited[c] ||
+                (sb.length() != 0 && sb.charAt(sb.length() - 1) == c)) {
+                continue;
             }
-        return "";
+            while ((sb.length() != 0 && sb.charAt(sb.length() - 1) > c) &&
+                    counts[sb.charAt(sb.length() - 1)] > 0) {
+                isVisited[sb.charAt(sb.length() - 1)] = false;
+                sb.deleteCharAt(sb.length() - 1);
+            }
+            sb.append(c);
+            isVisited[c] = true;
+        }
+        return sb.toString();
     }
 }
