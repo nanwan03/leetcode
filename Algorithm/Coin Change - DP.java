f9f947030bd278bfd769b5a2dc6e095918badfa3
nanwan03 <wn1842@gmail.com>
2015-12-26 23:43:27 -0500

    commit

---
+++ Algorithm/Coin Change - DP.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if(amount<1) return 0;
+        int L = coins.length;
+        int[] dp = new int[amount+1];
+        int sum = 0;
+    
+        while(++sum<=amount) {
+            int min = -1;
+            for(int coin : coins) {
+                if(sum >= coin && dp[sum-coin]!=-1) {
+                    int temp = dp[sum-coin]+1;
+                    min = min<0 ? temp : (temp < min ? temp : min);
+                }
+            }
+            dp[sum] = min;
+        }
+        return dp[amount];
+    }
+}
