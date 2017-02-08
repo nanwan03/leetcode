1ffa1d92b94897dacf48fab5188a2a53dbeae97f
nanwan03 <wn1842@gmail.com>
2017-02-08 16:07:21 -0500

    commit

--- Algorithm/Super Ugly Number.java
+++ Algorithm/Super Ugly Number.java
@@ -1,46 +1,35 @@
-import java.util.Comparator;
-import java.util.PriorityQueue;
-class Number{
-    int un;
-    int pos;
-    int prime;
-    Number(int un, int pos, int prime){
-        this.un = un;
-        this.pos = pos;
-        this.prime = prime;
-    }
-}
-class NumberCompare implements Comparator<Number>{
-
-    @Override
-    public int compare(Number x, Number y) {
-        // TODO Auto-generated method stub
-        if (x.un > y.un)
-            return 1;
-        else if (x.un < y.un)
-            return -1;
-        else
-            return 0;
-    }
-}
 public class Solution {
+    private class uglyNumber implements Comparable<uglyNumber> {
+        int val;
+        int index;
+        int prime;
+        public uglyNumber(int val, int index, int prime) {
+            this.val = val;
+            this.index = index;
+            this.prime = prime;
+        }
+        @Override
+        public int compareTo(uglyNumber num) {
+            return this.val - num.val;
+        }
+    }
     public int nthSuperUglyNumber(int n, int[] primes) {
-        Comparator<Number> comparator = new NumberCompare();
-        PriorityQueue<Number> queue = 
-                new PriorityQueue<Number>(primes.length, comparator);
-        for(int i = 0; i < primes.length; i ++) 
-            queue.add(new Number(primes[i], 0, primes[i]));
-        int[] uglyNums = new int[n];
-        uglyNums[0] = 1;
-        for(int i = 1; i < n; i++){
-            Number min = queue.peek();
-            uglyNums[i] = min.un;
-            while(queue.peek().un == min.un){
-                Number tmp = queue.poll();
-                queue.add(new Number(uglyNums[tmp.pos + 1] * tmp.prime, tmp.pos+1, tmp.prime)); 
+        List<Integer> rst = new ArrayList<Integer>();
+        if (n == 0 || primes == null || primes.length == 0) {
+            return 1;
+        }
+        Queue<uglyNumber> heap = new PriorityQueue<uglyNumber>();
+        for (int prime : primes) {
+            heap.offer(new uglyNumber(prime, 1, prime));
+        }
+        rst.add(1);
+        for (int i = 1; i < n; ++i) {
+            rst.add(heap.peek().val);
+            while (heap.peek().val == rst.get(i)) {
+                uglyNumber num = heap.poll();
+                heap.offer(new uglyNumber(rst.get(num.index) * num.prime, num.index + 1, num.prime));
             }
         }
-    
-        return uglyNums[n-1];
+        return rst.get(n - 1);
     }
 }
