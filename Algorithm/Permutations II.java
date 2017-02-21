1b72ddcf0efcba670a7dc9eb56af8da023f01683
nanwan03 <wn1842@gmail.com>
2017-02-21 10:57:02 -0500

    commit

--- Algorithm/Permutations II.java
+++ Algorithm/Permutations II.java
@@ -1,28 +1,29 @@
 public class Solution {
-    public List<List<Integer>> permuteUnique(int[] num) {
+    public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> rst = new ArrayList<List<Integer>>();
-        if (num == null || num.length == 0) {
+        if (nums == null || nums.length == 0) {
             return rst;
         }
-        List<Integer> items = new ArrayList<Integer>();
-        for (int i : num) {
-            items.add(i);
-        }
-        helper(rst, items, 0);
+        helper(rst, nums, 0);
         return rst;
     }
-    private void helper(List<List<Integer>> rst, List<Integer> items, int pos) {
-        if (pos == items.size()) {
-            rst.add(new ArrayList<Integer>(items));
+    private void helper(List<List<Integer>> rst, int[] nums, int pos) {
+        if (pos == nums.length) {
+            rst.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
             return;
         }
         Set<Integer> set = new HashSet<Integer>();
-        for (int i = pos; i < items.size(); i++) {
-            if (set.add(items.get(i))) {
-                Collections.swap(items, i, pos);
-                helper(rst, items, pos + 1);
-                Collections.swap(items, i, pos);
+        for (int i = pos; i < nums.length; ++i) {
+            if (set.add(nums[i])) {
+                swap(nums, pos, i);
+                helper(rst, nums, pos + 1);
+                swap(nums, pos, i);
             }
         }
+    }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
