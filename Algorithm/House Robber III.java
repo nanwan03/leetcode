5cb605b0153aa25e548fb9ee71f97e0d9b2c1f54
nanwan03 <wn1842@gmail.com>
2017-04-01 17:02:08 -0400

    commit

--- Algorithm/House Robber III.java
+++ Algorithm/House Robber III.java
@@ -8,21 +8,29 @@
  * }
  */
 public class Solution {
-    public int rob(TreeNode root) {
-        int[] result = findMax(root);
-        return Math.max(result[0], result[1]);
-    }
-
-    // returns int[2] result. 
-    // result[0] -- max value robbing current root; result[1] -- max value without robbing current root.
-    private int[] findMax(TreeNode root) {
-        if (root == null) {
-            return new int[] {0, 0};
+    private class ResultType {
+        int notSteal;
+        int steal;
+        ResultType(int notSteal, int steal) {
+            this.notSteal = notSteal;
+            this.steal = steal;
         }
-        int[] left = findMax(root.left);
-        int[] right = findMax(root.right);
-        int result0 = root.val + left[1] + right[1];  // rob current root
-        int result1 = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);  // not rob current root
-        return new int[] {result0, result1};
+    }
+    public int rob(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        ResultType rs = helper(root);
+        return Math.max(rs.notSteal, rs.steal);
+    }
+    private ResultType helper(TreeNode root) {
+        if (root == null) {
+            return new ResultType(0, 0);
+        }
+        ResultType left = helper(root.left);
+        ResultType right = helper(root.right);
+        int notSteal = Math.max(left.notSteal, left.steal) + Math.max(right.notSteal, right.steal);
+        int steal = left.notSteal + right.notSteal + root.val;
+        return new ResultType(notSteal, steal);
     }
 }
