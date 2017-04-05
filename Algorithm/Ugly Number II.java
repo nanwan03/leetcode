c81f3b26e45237315ea0663e77872d558ae86f41
nanwan03 <wn1842@gmail.com>
2017-04-05 15:39:23 -0400

    commit

--- Algorithm/Ugly Number II.java
+++ Algorithm/Ugly Number II.java
@@ -1,30 +1,21 @@
 public class Solution {
     public int nthUglyNumber(int n) {
-        if (n == 1) {
-            return 1;
-        }
-        Queue<Long> heap = new PriorityQueue<Long>();
-        heap.offer(2L);
-        heap.offer(3L);
-        heap.offer(5L);
+        Queue<Long> queue = new PriorityQueue<Long>();
         Set<Long> set = new HashSet<Long>();
-        set.add(2L);
-        set.add(3L);
-        set.add(5L);
-        --n;
-        while (n > 1) {
-            long cur = heap.poll();
-            if (set.add(cur * 2)) {
-                heap.offer(cur * 2);
+        queue.offer(1L);
+        set.add(1L);
+        while (--n > 0) {
+            Long val = queue.poll();
+            if (set.add(val * 2)) {
+                queue.offer(val * 2);
             }
-            if (set.add(cur * 3)) {
-                heap.offer(cur * 3);
-            } 
-            if (set.add(cur * 5)) {
-                heap.offer(cur * 5);
+            if (set.add(val * 3)) {
+                queue.offer(val * 3);
             }
-            --n;
+            if (set.add(val * 5)) {
+                queue.offer(val * 5);
+            }
         }
-        return heap.peek().intValue();
+        return queue.peek().intValue();
     }
 }
