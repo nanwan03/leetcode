54fa6a63ccfb7a738700694964ee4e95af87bcc6
nanwan03 <wn1842@gmail.com>
2017-07-04 22:38:35 -0700

    commit

--- Algorithm/Spiral Matrix II.java
+++ Algorithm/Spiral Matrix II.java
@@ -1,34 +1,24 @@
 public class Solution {
+    private int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     public int[][] generateMatrix(int n) {
         int[][] rst = new int[n][n];
         if (n == 0) {
             return rst;
         }
-        int left = 0;
-        int right = n - 1;
-        int top = 0;
-        int bottom = n - 1;
-        int k = 1;
-        while (left < right && top < bottom) {
-            for (int i = left; i < right; i++) {
-                rst[top][i] = k++;
+        int x = 0;
+        int y = 0;
+        int dir = 0;
+        for (int i = 1; i <= n * n; ++i) {
+            rst[x][y] = i;
+            int nx = x + dirs[dir][0];
+            int ny = y + dirs[dir][1];
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n || rst[nx][ny] != 0) {
+                dir = (dir + 1) % 4;
+                nx = x + dirs[dir][0];
+                ny = y + dirs[dir][1];
             }
-            for (int i = top; i < bottom; i++) {
-                rst[i][right] = k++;
-            }
-            for (int i = right; i > left; i--) {
-                rst[bottom][i] = k++;
-            }
-            for (int i = bottom; i > top; i--) {
-                rst[i][left] = k++;
-            }
-            left++;
-            right--;
-            top++;
-            bottom--;
-        }
-        if (n % 2 == 1) {
-            rst[n / 2][n / 2] = k;
+            x = nx;
+            y = ny;
         }
         return rst;
     }
