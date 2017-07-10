7eab2c542ebb0e8457c8c4ed0689cc3095a4ff02
nanwan03 <wn1842@gmail.com>
2017-07-10 15:56:46 -0700

    commit

--- Algorithm/Minimum Window Substring.java
+++ Algorithm/Minimum Window Substring.java
@@ -1,37 +1,32 @@
 public class Solution {
-    public String minWindow(String S, String T) {
-    	if (S == null || S.length() == 0 || T == null || T.length() == 0) {
-    		return "";
-    	}
-    	int[] total = new int[256];
-    	int[] found = new int[256];
-    	for (int i = 0; i < T.length(); i++) {
-    		total[T.charAt(i)]++;
-    	}
-    	int start = -1;
-    	int end = S.length();
-    	int left = 0;
-    	int windowSize = 0;
-    	for (int i = 0; i < S.length(); i++) {
-    		if (total[S.charAt(i)] != 0) {
-    			found[S.charAt(i)]++;
-    			if (found[S.charAt(i)] <= total[S.charAt(i)]) {
-    				windowSize++;
-    			}
-    			if (windowSize == T.length()) {
-    				while (total[S.charAt(left)] == 0 ||
-    						--found[S.charAt(left)] >= total[S.charAt(left)]) {
-    					left++;
-    				}
-    				if (i - left < end - start) {
-    					start = left;
-    					end = i;
-    				}
-    				left++;
-    				windowSize--;
-    			}
-    		}
-    	}
-    	return start == -1 ? "" : S.substring(start, end + 1);
+    public String minWindow(String s, String t) {
+        if (s == null || s.length() == 0 || t == null) {
+            return "";
+        }
+        int[] total = new int[256];
+        for (int i = 0; i < t.length(); ++i) {
+            total[t.charAt(i)]++;
+        }
+        int start = -1;
+        int end = s.length();
+        int left = 0;
+        int windowSize = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (--total[s.charAt(i)] >= 0) {
+                windowSize++;
+            }
+            if (windowSize == t.length()) {
+                while (++total[s.charAt(left)] <= 0) {
+                    left++;
+                }
+                if (i - left < end - start) {
+                    start = left;
+                    end = i;
+                }
+                left++;
+                windowSize--;
+            }
+        }
+        return start == -1 ? "" : s.substring(start, end + 1);
     }
 }
