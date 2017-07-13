e0a4e3b3055738d3a4ec29664845439b62351bc3
nanwan03 <wn1842@gmail.com>
2017-07-13 15:49:43 -0700

    commit

--- Algorithm/Scramble String.java
+++ Algorithm/Scramble String.java
@@ -8,16 +8,15 @@ public class Solution {
         }
         int size = s1.length();
         boolean[][][] dp = new boolean[size + 1][size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                dp[1][i][j] = s1.charAt(i) == s2.charAt(j);
-            }
-        }
-        for (int len = 2; len <= size; len++) {
-            for (int i = size - len; i >= 0; i--) {
-                for (int j = size - len; j >= 0; j--) {
-                    for (int k = 1; k < len && !dp[len][i][j]; k++) {
-                        dp[len][i][j] = (dp[k][i][j] && dp[len - k][i + k][j + k]) || (dp[k][i + len - k][j] && dp[len - k][i][j + k]);
+        for (int len = 1; len <= size; ++len) {
+            for (int i = size - len; i >= 0; --i) {
+                for (int j = size - len; j >= 0; --j) {
+                    if (len == 1) {
+                        dp[1][i][j] = s1.charAt(i) == s2.charAt(j);
+                    } else {
+                        for (int k = 1; k < len && !dp[len][i][j]; ++k) {
+                            dp[len][i][j] = (dp[k][i][j] && dp[len - k][i + k][j + k]) || (dp[k][i + len - k][j] && dp[len - k][i][j + k]);
+                        }
                     }
                 }
             }
