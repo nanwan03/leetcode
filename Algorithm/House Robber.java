66b41a609cffabec9691b69ba2ba2000a7b1f982
nanwan03 <wn1842@gmail.com>
2017-07-17 15:20:31 -0700

    commit

--- Algorithm/House Robber.java
+++ Algorithm/House Robber.java
@@ -3,14 +3,15 @@ public class Solution {
         if (nums == null || nums.length == 0) {
             return 0;
         }
-        if (nums.length == 1) {
+        int size = nums.length;
+        if (size == 1) {
             return nums[0];
         }
-        int[] dp = new int[]{nums[0], Math.max(nums[0], nums[1]), 0};
+        int[] dp = new int[]{nums[0], Math.max(nums[0], nums[1])};
         for (int i = 2; i < nums.length; ++i) {
-            dp[2] = Math.max(dp[0] + nums[i], dp[1]);
+            int temp = Math.max(dp[0] + nums[i], dp[1]);
             dp[0] = dp[1];
-            dp[1] = dp[2];
+            dp[1] = temp;
         }
         return dp[1];
     }
