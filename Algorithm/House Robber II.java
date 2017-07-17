7f8c35e1cd10e2ebd1c4632e09351d4f63798039
nanwan03 <wn1842@gmail.com>
2017-07-17 16:22:36 -0700

    commit

--- Algorithm/House Robber II.java
+++ Algorithm/House Robber II.java
@@ -3,20 +3,21 @@ public class Solution {
         if (nums == null || nums.length == 0) {
             return 0;
         }
-        if (nums.length == 1) {
+        int size = nums.length;
+        if (size == 1) {
             return nums[0];
         }
-        return Math.max(helper(nums, 0, nums.length - 2), helper(nums, 1, nums.length - 1));
+        return Math.max(helper(nums, 0, size - 2), helper(nums, 1, size - 1));
     }
     private int helper(int[] nums, int start, int end) {
         if (start == end) {
             return nums[start];
         }
-        int[] dp = new int[]{nums[start], Math.max(nums[start], nums[start + 1]), 0};
+        int[] dp = new int[]{nums[start], Math.max(nums[start], nums[start + 1])};
         for (int i = start + 2; i <= end; ++i) {
-            dp[2] = Math.max(dp[0] + nums[i], dp[1]);
+            int temp = Math.max(dp[0] + nums[i], dp[1]);
             dp[0] = dp[1];
-            dp[1] = dp[2];
+            dp[1] = temp;
         }
         return dp[1];
     }
