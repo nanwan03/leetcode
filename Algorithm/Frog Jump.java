ae16ff7070e470e7571a4a849a3e8d9c70feeab9
nanwan03 <wn1842@gmail.com>
2017-08-05 22:26:50 -0700

    commit

---
+++ Algorithm/Frog Jump.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public boolean canCross(int[] stones) {
+        if (stones.length <= 1) {
+        	return true;
+        }
+        
+        Map<Integer, Set<Integer>> map = new HashMap<Integer, Set<Integer>>();
+        for (int i = 0; i < stones.length; i++) {
+        	map.put(stones[i], new HashSet<Integer>() );
+        }
+        map.get(0).add(1);
+        
+        for (int i = 0; i < stones.length - 1; i++) {
+        	int stone = stones[i];
+        	for (int step : map.get(stone)) {
+        		int reach = step + stone;
+        		if (reach == stones[stones.length - 1]) {
+        			return true;
+        		}
+        		if (map.containsKey(reach)) {
+            		Set<Integer> set = map.get(reach);
+            		set.add(step);
+            		if (step - 1 > 0) {
+            		    set.add(step - 1);
+            		}
+            		set.add(step + 1);
+        		}
+        	}
+        }
+        return false;
+    }
+}
