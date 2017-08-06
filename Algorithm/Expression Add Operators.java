8b516652e69ba890a894cc366bc5d6b9dc98a00c
nanwan03 <wn1842@gmail.com>
2017-08-06 16:01:04 -0700

    commit

--- Algorithm/Expression Add Operators.java
+++ Algorithm/Expression Add Operators.java
@@ -1,25 +1,28 @@
 public class Solution {
     public List<String> addOperators(String num, int target) {
         List<String> rst = new ArrayList<String>();
-        if(num == null || num.length() == 0) return rst;
+        if (num == null || num.length() == 0) {
+            return rst;
+        }
         helper(rst, "", num, target, 0, 0, 0);
         return rst;
     }
-    public void helper(List<String> rst, String path, String num, int target, int pos, int eval, int multed){
-        if(pos == num.length()){
-            if(target == eval)
+    private void helper(List<String> rst, String path, String num, int target, int pos, long eval, long multed){
+        if (pos == num.length()) {
+            if (target == eval) {
                 rst.add(path);
+            }
             return;
         }
-        for(int i = pos; i < num.length(); i++){
-            if(i != pos && num.charAt(pos) == '0') break;
-            long curLong = Long.parseLong(num.substring(pos, i + 1));
-            if(curLong > Integer.MAX_VALUE || -curLong < Integer.MIN_VALUE) break;
-            int cur = (int)curLong;
-            if(pos == 0){
-                helper(rst, path + cur, num, target, i + 1, cur, cur);
+        for (int i = pos; i < num.length(); i++) {
+            if (i != pos && num.charAt(pos) == '0') {
+                break;
             }
-            else{
+            long cur = Long.parseLong(num.substring(pos, i + 1));
+            if(pos == 0) {
+                helper(rst, cur + "", num, target, i + 1, cur, cur);
+            }
+            else {
                 helper(rst, path + "+" + cur, num, target, i + 1, eval + cur , cur);
                 helper(rst, path + "-" + cur, num, target, i + 1, eval -cur, -cur);
                 helper(rst, path + "*" + cur, num, target, i + 1, eval - multed + multed * cur, multed * cur );
