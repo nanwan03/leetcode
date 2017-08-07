a4eddf1983d03b2a8b8973c3eaae3d8f2ffa87e9
nanwan03 <wn1842@gmail.com>
2017-08-06 17:20:50 -0700

    commit

---
+++ Guess Number Higher or Lower II.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int getMoneyAmount(int n) {
+        int[][] dp = new int[n + 1][n + 1];
+        for (int l = 2; l <= n; l++) {
+            for (int i = 1; i <= n - (l - 1); i++) {
+                int j = i + (l - 1);
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int g = i; g <= j; g++) {
+                    int cost;
+                    if (g == n) {
+                        cost = dp[i][g - 1] + g;
+                    } else {
+                        cost = g + Math.max(dp[i][g - 1], dp[g + 1][j]);
+                    }
+                    dp[i][j] = Math.min(dp[i][j], cost);
+                }
+            }
+        }
+        return dp[1][n];
+    }
+}
