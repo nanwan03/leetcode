007aa0d391922c4bc1f316d8395dc5b85abfc829
nanwan03 <wn1842@gmail.com>
2017-08-06 20:02:05 -0700

    commit

--- Algorithm/Longest Increasing Subsequence.java
+++ Algorithm/Longest Increasing Subsequence.java
@@ -3,10 +3,7 @@ public class Solution {
         int[] dp = new int[nums.length];
         int rst = 0;
         for(int i : nums) {
-            int index = Arrays.binarySearch(dp, 0, rst, i);
-            if(index < 0) {
-                index = -(index + 1);
-            }
+            int index = search(dp, 0, rst, i);
             dp[index] = i;
             if(index == rst) {
                 rst++;
@@ -14,5 +11,28 @@ public class Solution {
         }
 
         return rst;
+    }
+    private int search(int[] A, int left, int right, int target) {
+        if (right == 0) {
+            return 0;
+        }
+        right = right - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (A[mid] == target) {
+                return mid;
+            } else if (A[mid] < target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        if (A[right] < target) {
+            return right + 1;
+        } else if (A[right] == target || A[left] < target) {
+            return right;
+        } else {
+            return left;
+        }
     }
 }
--- Algorithm/Russian Doll Envelopes.java
+++ Algorithm/Russian Doll Envelopes.java
@@ -9,15 +9,35 @@ public class Solution {
         int rst = 0;
         int dp[] = new int[envelopes.length];
         for(int[] envelope : envelopes){
-            int index = Arrays.binarySearch(dp, 0, rst, envelope[1]);
-            if(index < 0) {
-                index = -(index + 1);
-            }
+            int index = search(dp, 0, rst, envelope[1]);
             dp[index] = envelope[1];
             if(index == rst) {
                 rst++;
             }
         }
         return rst;
+    }
+    private int search(int[] A, int left, int right, int target) {
+        if (right == 0) {
+            return 0;
+        }
+        right = right - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (A[mid] == target) {
+                return mid;
+            } else if (A[mid] < target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        if (A[right] < target) {
+            return right + 1;
+        } else if (A[right] == target || A[left] < target) {
+            return right;
+        } else {
+            return left;
+        }
     }
 }
