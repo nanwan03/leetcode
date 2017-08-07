a49f578120285466544aa8ca5868c5841e14fdda
nanwan03 <wn1842@gmail.com>
2017-08-06 17:53:23 -0700

    commit

--- Algorithm/Longest Increasing Subsequence.java
+++ Algorithm/Longest Increasing Subsequence.java
@@ -1,19 +1,18 @@
 public class Solution {
     public int lengthOfLIS(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
         int[] dp = new int[nums.length];
-        int longest = 1;
-        Arrays.fill(dp, 1);
-        for (int i = 1; i < nums.length; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                    longest = Math.max(longest, dp[i]);
-                }
+        int rst = 0;
+        for(int i : nums) {
+            int index = Arrays.binarySearch(dp, 0, rst, i);
+            if(index < 0) {
+                index = -(index + 1);
+            }
+            dp[index] = i;
+            if(index == rst) {
+                rst++;
             }
         }
-        return longest;
+
+        return rst;
     }
 }
---
+++ Algorithm/Russian Doll Envelopes.java
@@ -0,0 +1,23 @@
+public class Solution {
+    private class Cmp implements Comparator<int[]> {
+        public int compare(int[] a, int[] b) {
+            return a[0] == b[0] ? b[1] - a[1] : a[0] - b[0];
+        }
+    }
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, new Cmp());
+        int rst = 0;
+        int dp[] = new int[envelopes.length];
+        for(int[] envelope : envelopes){
+            int index = Arrays.binarySearch(dp, 0, rst, envelope[1]);
+            if(index < 0) {
+                index = -(index + 1);
+            }
+            dp[index] = envelope[1];
+            if(index == rst) {
+                rst++;
+            }
+        }
+        return rst;
+    }
+}
