650609a304cc14d79125ac017bbd2322d8e1f4ce
nanwan03 <wn1842@gmail.com>
2017-08-06 18:25:08 -0700

    commit

--- Algorithm/Word Break II.java
+++ Algorithm/Word Break II.java
@@ -1,32 +1,25 @@
 public class Solution {
-    public List<String> wordBreak(String s, Set<String> dict) {
-        List<String> rst = new ArrayList<String>();
-        if (dict == null || dict.size() == 0) {
-        	return rst;
-        }
-        Map<String, List<String>> map = new HashMap<String, List<String>>();
-        return helper(map, s, dict);
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        return DFS(s, new HashSet<String>(wordDict), new HashMap<String, List<String>>());
     }
-    private List<String> helper(Map<String, List<String>> map, String s, Set<String> dict) {
-    	if (map.containsKey(s)) {
-    		return map.get(s);
-    	}
-    	List<String> rst = new ArrayList<String>();
-    	for (int i = 0; i < s.length(); i++) {
-    		String firstPart = s.substring(0, i + 1);
-    		if (dict.contains(firstPart)) {
-    			String secondPart = s.substring(i + 1);
-    			if (secondPart.length() == 0) {
-    				rst.add(firstPart);
-    			} else {
-    				List<String> items = helper(map, secondPart, dict);
-    				for (String str : items) {
-    					rst.add(firstPart+" "+str);
-    				}
-    			}
-    		}
-    	}
-    	map.put(s, rst);
-    	return rst;
+    private List<String> DFS(String s, Set<String> wordDict, Map<String, List<String>>map) {
+        if (map.containsKey(s)) {
+            return map.get(s);
+        }
+        List<String> rst = new ArrayList<String>();  
+        for (String word : wordDict) {
+            if (s.startsWith(word)) {
+                if (word.equals(s)) {
+                    rst.add(word);
+                } else {
+                    List<String> sublist = DFS(s.substring(word.length()), wordDict, map);
+                    for (String sub : sublist) {
+                        rst.add(word + " " + sub);               
+                    }
+                }
+            }
+        }       
+        map.put(s, rst);
+        return rst;
     }
 }
