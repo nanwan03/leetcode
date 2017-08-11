d4ece6342cac510ef046d5fd0087c16836f05475
nanwan03 <wn1842@gmail.com>
2017-08-10 22:04:38 -0700

    commit

---
+++ Algorithm/Is Subsequence.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public boolean isSubsequence(String s, String t) {
+        List<Integer>[] idx = new List[256];
+        for (int i = 0; i < t.length(); i++) {
+            if (idx[t.charAt(i)] == null) {
+                idx[t.charAt(i)] = new ArrayList<>();
+            }
+            idx[t.charAt(i)].add(i);
+        }
+        int prev = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (idx[s.charAt(i)] == null) {
+                return false;
+            }
+            int j = Collections.binarySearch(idx[s.charAt(i)], prev);
+            if (j < 0) {
+                j = -j - 1;
+            }
+            if (j == idx[s.charAt(i)].size()) {
+                return false;
+            }
+            prev = idx[s.charAt(i)].get(j) + 1;
+        }
+        return true;
+    }
+}
---
+++ Algorithm/Mini Parser.java
@@ -0,0 +1,65 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+public class Solution {
+    public NestedInteger deserialize(String s) {
+        NestedInteger res = new NestedInteger();
+        Stack<NestedInteger> stack = new Stack<NestedInteger>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '[') {
+                stack.push(new NestedInteger());
+                continue;
+            }
+            if (c == ',') {
+                continue;
+            }
+            if (c == ']') {
+                NestedInteger temp = stack.pop();
+                if (!stack.isEmpty()) {
+                    stack.peek().add(temp);
+                } else {
+                    stack.push(temp);
+                }
+                continue;
+            }
+            int start = i;
+            while (i + 1 < s.length() && (Character.isDigit(s.charAt(i + 1)) || s.charAt(i + 1) == '-')) {
+                i++;
+            }
+            int num = Integer.parseInt(s.substring(start, i + 1));
+            NestedInteger ni = new NestedInteger(num);
+            if (!stack.isEmpty()) {
+                stack.peek().add(ni);
+            } else {
+                stack.push(ni);
+            }
+        }
+        return stack.peek();
+    }
+}
