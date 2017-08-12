3c4e57127926113d41e223aa7930a8d0f019bb84
nanwan03 <wn1842@gmail.com>
2017-08-11 18:29:10 -0700

    commit

---
+++ Algorithm/Unique Substrings in Wraparound String.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int findSubstringInWraproundString(String p) {
+        int[] count = new int[26];
+        int maxLength = 0; 
+        for (int i = 0; i < p.length(); ++i) {
+            if (i > 0 && (p.charAt(i) - p.charAt(i - 1) == 1 || (p.charAt(i - 1) - p.charAt(i) == 25))) {
+                maxLength++;
+            }
+            else {
+                maxLength = 1;
+            }
+            
+            int index = p.charAt(i) - 'a';
+            count[index] = Math.max(count[index], maxLength);
+        }
+        
+        int sum = 0;
+        for (int i = 0; i < 26; i++) {
+            sum += count[i];
+        }
+        return sum;
+    }
+}
