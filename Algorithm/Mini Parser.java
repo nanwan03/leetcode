081d46695ba4f710f541b0199d78fa7f599f8ddc
nanwan03 <wn1842@gmail.com>
2017-08-14 17:01:26 -0700

    commit

--- Algorithm/Mini Parser.java
+++ Algorithm/Mini Parser.java
@@ -28,36 +28,28 @@
  */
 public class Solution {
     public NestedInteger deserialize(String s) {
-        NestedInteger res = new NestedInteger();
         Stack<NestedInteger> stack = new Stack<NestedInteger>();
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); ++i) {
             char c = s.charAt(i);
             if (c == '[') {
                 stack.push(new NestedInteger());
-                continue;
-            }
-            if (c == ',') {
-                continue;
-            }
-            if (c == ']') {
-                NestedInteger temp = stack.pop();
-                if (!stack.isEmpty()) {
-                    stack.peek().add(temp);
-                } else {
-                    stack.push(temp);
+            } else if (c == ']') {
+                if (stack.size() > 1) {
+                    NestedInteger ni = stack.pop();
+                    stack.peek().add(ni);
                 }
-                continue;
-            }
-            int start = i;
-            while (i + 1 < s.length() && (Character.isDigit(s.charAt(i + 1)) || s.charAt(i + 1) == '-')) {
-                i++;
-            }
-            int num = Integer.parseInt(s.substring(start, i + 1));
-            NestedInteger ni = new NestedInteger(num);
-            if (!stack.isEmpty()) {
-                stack.peek().add(ni);
-            } else {
-                stack.push(ni);
+            } else if (c != ',') {
+                int start = i;
+                while (i + 1 < s.length() && (Character.isDigit(s.charAt(i + 1)) || s.charAt(i + 1) == '-')) {
+                    i++;
+                }
+                int num = Integer.parseInt(s.substring(start, i + 1));
+                NestedInteger temp = new NestedInteger(num);
+                if (stack.isEmpty()) {
+                    stack.push(temp);
+                } else {
+                    stack.peek().add(temp);
+                }
             }
         }
         return stack.peek();
