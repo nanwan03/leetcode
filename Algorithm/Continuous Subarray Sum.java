252c398bf27ebe760e07e817c910fe3fc76dc509
nanwan03 <wn1842@gmail.com>
2017-08-16 16:24:53 -0700

    commit

--- Algorithm/Continuous Subarray Sum.java
+++ Algorithm/Continuous Subarray Sum.java
@@ -1,20 +1,22 @@
 public class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        int sum = 0;
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         map.put(0, -1);
-        int sum = 0;
-        for (int i = 0;i < nums.length; ++i) {
+        for (int i = 0; i < nums.length; ++i) {
             sum += nums[i];
             if (k != 0) {
-                sum %= k; 
+                sum %= k;
             }
-            Integer prev = map.get(sum);
-            if (prev != null) {
-                if (i - prev > 1) {
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
+            } else {
+                if (i - map.get(sum) > 1) {
                     return true;
                 }
-            } else {
-                map.put(sum, i);
             }
         }
         return false;
