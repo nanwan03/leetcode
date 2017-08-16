18066e3173cde4d7f279b0d0b3e1e2a1abb71dab
nanwan03 <wn1842@gmail.com>
2017-08-16 16:30:22 -0700

    commit

--- Algorithm/Maximum Average Subarray I.java
+++ Algorithm/Maximum Average Subarray I.java
@@ -1,15 +1,19 @@
 public class Solution {
     public double findMaxAverage(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
         long sum = 0;
-        for (int i = 0; i < k; i++) {
-            sum += nums[i];
+        long max = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (i < k) {
+                sum += nums[i];
+                max = sum;
+            } else {
+                sum += nums[i] - nums[i - k];
+                max = Math.max(max, sum);
+            }
         }
-        long max = sum;
-        for (int i = k; i < nums.length; i++) {
-            sum += nums[i] - nums[i - k];
-            max = Math.max(max, sum);
-        }
-        
         return (double)max / (double)k;
     }
 }
