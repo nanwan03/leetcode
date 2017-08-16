9764c25eacefe632cb7fb7b52a2afa9545289f37
nanwan03 <wn1842@gmail.com>
2017-08-16 16:34:09 -0700

    commit

--- Algorithm/Island Perimeter.java
+++ Algorithm/Island Perimeter.java
@@ -1,22 +1,24 @@
 public class Solution {
     public int islandPerimeter(int[][] grid) {
-        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+        if (grid == null || grid.length == 0) {
             return 0;
         }
-        int result = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+        int rst = 0;
+        int row = grid.length;
+        int col = grid[0].length;
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
                 if (grid[i][j] == 1) {
-                    result += 4;
-                    if (i > 0 && grid[i-1][j] == 1) {
-                        result -= 2;
+                    rst += 4;
+                    if (i > 0 && grid[i - 1][j] == 1) {
+                        rst -= 2;
                     }
-                    if (j > 0 && grid[i][j-1] == 1) {
-                        result -= 2;
+                    if (j > 0 && grid[i][j - 1] == 1) {
+                        rst -= 2;
                     }
                 }
             }
         }
-        return result;
+        return rst;
     }
 }
