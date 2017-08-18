f8041131e5fd5adc0f3a6e8d69986b3fc537b3e1
nanwan03 <wn1842@gmail.com>
2017-08-17 21:53:36 -0700

    commit

--- Algorithm/2 Keys Keyboard.java
+++ Algorithm/2 Keys Keyboard.java
@@ -1,13 +1,14 @@
-public class Solution {
+class Solution {
     public int minSteps(int n) {
         if (n == 0) {
             return 0;
         }
         int[] dp = new int[n + 1];
         for (int i = 2; i <= n; ++i) {
-            for (int j = i - 1; j >= 1; --j) {
+            dp[i] = i;
+            for (int j = i / 2; j > 1; --j) {
                 if (i % j == 0) {
-                    dp[i] = dp[j] + i / j;
+                    dp[i] = dp[j] + (i / j);
                     break;
                 }
             }
