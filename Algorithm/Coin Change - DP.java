81736d1b88a394d73423d57c94b5f75d58f8ccfd
nanwan03 <wn1842@gmail.com>
2017-08-21 09:28:30 -0700

    commit

--- Algorithm/Coin Change - DP.java
+++ Algorithm/Coin Change - DP.java
@@ -1,14 +1,14 @@
-public class Solution {
+class Solution {
     public int coinChange(int[] coins, int amount) {
-        if (coins == null || coins.length == 0 || amount == 0) {
-            return 0;
+        if (coins == null || coins.length == 0) {
+            return -1;
         }
         int[] dp = new int[amount + 1];
         Arrays.fill(dp, 1, dp.length, Integer.MAX_VALUE);
-        for (int i = 0; i < coins.length; ++i) {
-            for (int j = coins[i]; j <= amount; ++j) {
-                if (dp[j - coins[i]] != Integer.MAX_VALUE) {
-                    dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+        for (int i = 1; i <= amount; ++i) {
+            for (int j = 0; j < coins.length; ++j) {
+                if (i >= coins[j] && dp[i - coins[j]] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
                 }
             }
         }
