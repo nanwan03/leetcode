e0030023ac01f90f05e54e7bbb237ea29cc8f204
nanwan03 <wn1842@gmail.com>
2017-08-29 11:01:58 -0700

    commit

--- Algorithm/Best Time to Buy and Sell Stock IV - Solution I.java
+++ Algorithm/Best Time to Buy and Sell Stock IV - Solution I.java
@@ -1,26 +1,25 @@
-public class Solution {
+class Solution {
     public int maxProfit(int k, int[] prices) {
         if (prices == null || prices.length == 0) {
-        	return 0;
+            return 0;
         }
-        if (k >= prices.length / 2) {
-        	return quickSolve(prices);
+        int size = prices.length;
+        if (k >= size / 2) {
+            int rst = 0;
+            int min = prices[0];
+            for (int i = 1; i < prices.length; ++i) {
+                rst += Math.max(0, prices[i] - prices[i - 1]);
+            }
+            return rst;
         }
-        int[][] dp = new int[k + 1][prices.length];
-        for (int i = 1; i <= k; i++) {
-        	int temp = -prices[0];
-        	for (int j = 1; j < prices.length; j++) {
-        		dp[i][j] = Math.max(dp[i][j - 1], temp + prices[j]);
-        		temp = Math.max(temp, dp[i - 1][j - 1] - prices[j]);
-        	}
+        int[][] dp = new int[k + 1][size];
+        for (int i = 1; i <= k; ++i) {
+            int localMax = dp[i - 1][0] - prices[0];
+            for (int j = 1; j < size; ++j) {
+                dp[i][j] = Math.max(dp[i][j - 1], localMax + prices[j]);
+                localMax = Math.max(localMax, dp[i - 1][j] - prices[j]);
+            }
         }
-        return dp[k][prices.length - 1];
-    }
-    private int quickSolve(int[] prices) {
-    	int rst = 0;
-    	for (int i = 1; i < prices.length; i++) {
-    		rst += Math.max(0,  prices[i] - prices[i - 1]);
-    	}
-    	return rst;
+        return dp[k][size - 1];
     }
 }
