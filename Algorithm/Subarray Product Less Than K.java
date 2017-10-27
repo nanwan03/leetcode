162fef101cf1cd5263d28f8ef6313078df8677e8
nanwan03 <wn1842@gmail.com>
2017-10-26 22:19:39 -0700

    commit

--- Algorithm/Subarray Product Less Than K.java
+++ Algorithm/Subarray Product Less Than K.java
@@ -1,17 +1,20 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        if (k < 2) {
-                return 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int product = 1;
+        int rst = 0;
+        int left = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            product *= nums[i];
+            while (left <= i && product >= k) {
+                product /= nums[left++];
             }
-            int result = 0;
-            int product = 1;
-            for (int i = 0, right = 0; right < nums.length; right++) {
-                product *= nums[right];
-                while (i < nums.length && product >= k) {
-                    product /= nums[i++];
-                }
-                result += right - i + 1;
+            if (product < k) {
+                rst += i - left + 1;
             }
-            return result;
+        }
+        return rst;
     }
 }
