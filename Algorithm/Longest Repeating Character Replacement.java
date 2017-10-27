ccb1863ae27534cbfffa36f3cf51a25cdbc8f440
nanwan03 <wn1842@gmail.com>
2017-10-26 22:47:13 -0700

    commit

--- Algorithm/Longest Repeating Character Replacement.java
+++ Algorithm/Longest Repeating Character Replacement.java
@@ -1,20 +1,19 @@
-public class Solution {
+class Solution {
     public int characterReplacement(String s, int k) {
         if (s == null || s.length() == 0) {
             return 0;
         }
-        int size = s.length();
-        int[] count = new int[26];
+        int windowSize = 0;
         int left = 0;
-        int max = 0;
         int rst = 0;
-        for (int i = 0; i < size; ++i) {
-            max = Math.max(max, ++count[s.charAt(i) - 'A']);
-            while (i - left + 1 > k + max) {
-                count[s.charAt(left) - 'A']--;
-                left++;
+        int[] count = new int[256];
+        for (int i = 0; i < s.length(); ++i) {
+            windowSize = Math.max(windowSize, ++count[s.charAt(i)]);
+            if (i + 1 - left > windowSize + k) {
+                --count[s.charAt(left++)];
+            } else {
+                rst = Math.max(rst, i + 1 - left);
             }
-            rst = Math.max(rst, i - left + 1);
         }
         return rst;
     }
