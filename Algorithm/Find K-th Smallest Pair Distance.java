878270be14ad447c726ed3b18e72c23a4e3146f6
nanwan03 <wn1842@gmail.com>
2017-10-29 10:38:02 -0700

    commit

---
+++ Algorithm/Find K-th Smallest Pair Distance.java
@@ -0,0 +1,35 @@
+class Solution {
+    private int countPairs(int[] a, int mid) {
+        int n = a.length, res = 0;
+        for (int i = 0; i < n; ++i) {
+            int j = i;
+            while (j < n && a[j] - a[i] <= mid) j++;
+            res += j - i - 1;
+        }
+        return res;
+    }
+
+    public int smallestDistancePair(int a[], int k) {
+        int n = a.length;
+        Arrays.sort(a);
+
+        // Minimum absolute difference
+        int low = a[1] - a[0];
+        for (int i = 1; i < n - 1; i++)
+            low = Math.min(low, a[i + 1] - a[i]);
+
+        // Maximum absolute difference
+        int high = a[n - 1] - a[0];
+
+        // Do binary search for k-th absolute difference
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (countPairs(a, mid) < k)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+
+        return low;
+    }
+}
