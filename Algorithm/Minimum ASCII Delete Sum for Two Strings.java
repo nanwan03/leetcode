650ca0b9f3878b14583d19f229d3eeed1004e74f
nanwan03 <wn1842@gmail.com>
2017-10-31 17:07:05 -0700

    commit

--- Algorithm/Minimum ASCII Delete Sum for Two Strings.java
+++ Algorithm/Minimum ASCII Delete Sum for Two Strings.java
@@ -1,23 +1,23 @@
 class Solution {
     public int minimumDeleteSum(String s1, String s2) {
-        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
-
-        for (int i = s1.length() - 1; i >= 0; i--) {
-            dp[i][s2.length()] = dp[i+1][s2.length()] + s1.codePointAt(i);
+        int l1 = s1.length();
+        int l2 = s2.length();
+        int[][] dp = new int[l1 + 1][l2 + 1];
+        for (int i = 1; i <= l1; ++i) {
+            dp[i][0] = dp[i - 1][0] + s1.codePointAt(i - 1); 
         }
-        for (int j = s2.length() - 1; j >= 0; j--) {
-            dp[s1.length()][j] = dp[s1.length()][j+1] + s2.codePointAt(j);
+        for (int j = 1; j <= l2; ++j) {
+            dp[0][j] = dp[0][j - 1] + s2.codePointAt(j - 1);
         }
-        for (int i = s1.length() - 1; i >= 0; i--) {
-            for (int j = s2.length() - 1; j >= 0; j--) {
-                if (s1.charAt(i) == s2.charAt(j)) {
-                    dp[i][j] = dp[i+1][j+1];
+        for (int i = 1; i <= l1; ++i) {
+            for (int j = 1; j <= l2; ++j) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = Math.min(dp[i+1][j] + s1.codePointAt(i),
-                                        dp[i][j+1] + s2.codePointAt(j));
+                    dp[i][j] = Math.min(dp[i - 1][j] + s1.codePointAt(i - 1), dp[i][j - 1] + s2.codePointAt(j - 1));
                 }
             }
         }
-        return dp[0][0];
+        return dp[l1][l2];
     }
 }
