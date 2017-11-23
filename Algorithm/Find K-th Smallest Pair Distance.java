f460cab63cd18205c7e435f45e09a0506ee91078
nanwan03 <wn1842@gmail.com>
2017-11-23 11:41:34 -0800

    commit

--- Algorithm/Find K-th Smallest Pair Distance.java
+++ Algorithm/Find K-th Smallest Pair Distance.java
@@ -1,35 +1,32 @@
 class Solution {
-    private int countPairs(int[] a, int mid) {
-        int n = a.length, res = 0;
-        for (int i = 0; i < n; ++i) {
-            int j = i;
-            while (j < n && a[j] - a[i] <= mid) j++;
-            res += j - i - 1;
+    public int smallestDistancePair(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return 0;
         }
-        return res;
+        Arrays.sort(nums);
+        int left = Integer.MAX_VALUE;
+        for (int i = 1; i < nums.length; ++i) {
+            left = Math.min(left, nums[i] - nums[i - 1]);
+        }
+        int right = nums[nums.length - 1] - nums[0];
+        while (left + 1 < right) {
+            int mid = (left + right) >>> 1;
+            if (getCount(nums, mid) >= k) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return getCount(nums, left) >= k ? left : right;
     }
-
-    public int smallestDistancePair(int a[], int k) {
-        int n = a.length;
-        Arrays.sort(a);
-
-        // Minimum absolute difference
-        int low = a[1] - a[0];
-        for (int i = 1; i < n - 1; i++)
-            low = Math.min(low, a[i + 1] - a[i]);
-
-        // Maximum absolute difference
-        int high = a[n - 1] - a[0];
-
-        // Do binary search for k-th absolute difference
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            if (countPairs(a, mid) < k)
-                low = mid + 1;
-            else
-                high = mid;
+    private int getCount(int[] array, int target) {
+        int size = array.length;
+        int rst = 0;
+        for (int i = 0; i <  size; ++i) {
+            for (int j = i + 1; j < size && array[j] - array[i] <= target; ++j) {
+                rst++;
+            }
         }
-
-        return low;
+        return rst;
     }
 }
