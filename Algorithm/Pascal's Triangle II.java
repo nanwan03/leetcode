acc1d7578d4f3ea00a62658b393bf3744acb3eed
nanwan03 <wn1842@gmail.com>
2018-04-29 23:06:23 -0700

    commit

--- Algorithm/Pascal's Triangle II.java
+++ Algorithm/Pascal's Triangle II.java
@@ -1,24 +1,17 @@
-public class Solution {
+class Solution {
     public List<Integer> getRow(int rowIndex) {
-        ArrayList<Integer> rst = new ArrayList<Integer>();
-        rowIndex += 1;
-        if (rowIndex == 0) {
-            return rst;
-        }
-
-        rst.add(1);
-        for (int i = 1; i < rowIndex; i++) {
-            ArrayList<Integer> tmp = new ArrayList<Integer>(i+1);
-            for (int j = 0; j < i + 1; j++) {
-                tmp.add(-1);
+        List<Integer> prev = Arrays.asList(new Integer[]{1});
+        while (--rowIndex >= 0) {
+            List<Integer> cur = new ArrayList<Integer>();
+            for (int i = 0; i <= prev.size(); ++i) {
+                if (i == 0 || i == prev.size()) {
+                    cur.add(1);
+                } else {
+                    cur.add(prev.get(i) + prev.get(i - 1));
+                }
             }
-            tmp.set(0, rst.get(0));
-            tmp.set(i, rst.get(i - 1));
-            for (int j = 1; j < i; j++) {
-                tmp.set(j, rst.get(j - 1) + rst.get(j));
-            }
-            rst = tmp;
+            prev = cur;
         }
-        return rst;
+        return prev;
     }
 }
