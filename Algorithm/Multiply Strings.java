5bab328f160c51491268c9a2c022276f83641c70
nanwan03 <wn1842@gmail.com>
2018-04-30 22:34:37 -0700

    commit

--- Algorithm/Multiply Strings.java
+++ Algorithm/Multiply Strings.java
@@ -1,18 +1,17 @@
-public class Solution {
+class Solution {
     public String multiply(String num1, String num2) {
-        int length = num1.length() + num2.length();
         int length1 = num1.length();
         int length2 = num2.length();
-        int[] rst = new int[length1 + length2];
-        for (int i = length1 - 1; i >= 0; i--) {
+        int length = length1 + length2;
+        int[] rst = new int[length];
+        for (int i = length1 - 1; i >= 0; --i) {
             int carry = 0;
-            int j = length2 - 1;
-            for (;j >= 0; j--) {
+            for(int j = length2 - 1; j >= 0; --j) {
                 int product = carry + rst[i + j + 1] + (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
                 carry = product / 10;
                 rst[i + j + 1] = product % 10;
             }
-            rst[i + j + 1] = carry;
+            rst[i] = carry;
         }
         int index = 0;
         StringBuilder sb = new StringBuilder();
