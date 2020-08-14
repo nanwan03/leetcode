643a4ff4a3bb797c3110170a08b25511a53ae213
nanwan03 <wn1842@gmail.com>
2020-08-13 22:07:43 -0700

    Create Subsets II.js
    commit

---
+++ Algorithm/JavaScript/Subsets II.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function(nums) {
+    let rst = [];
+    nums = nums.sort((a, b)=>(a - b));
+    dfs([], 0);
+    
+    function dfs(current, index) {
+        rst.push(current);
+        for (let i = index; i < nums.length; ++i) {
+            if (i > index && nums[i] === nums[i - 1]) {
+                continue;
+            }
+            dfs(current.concat(nums[i]), i + 1);
+        }
+    }
+    
+    return rst;
+};
