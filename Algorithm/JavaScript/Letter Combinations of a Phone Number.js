773ad46c25b941761e2ce81c4105b690c0405f52
nanwan03 <wn1842@gmail.com>
2020-08-14 22:45:42 -0700

    commit

---
+++ Algorithm/JavaScript/Letter Combinations of a Phone Number.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    if (digits.length === 0) {
+        return [];
+    }
+    const map = {
+        2: 'abc',
+        3: 'def',
+        4: 'ghi',
+        5: 'jkl',
+        6: 'mno',
+        7: 'pqrs',
+        8: 'tuv',
+        9: 'wxyz',
+    };
+
+    let rst = [];
+
+    let dfs = (current, index) => {
+        if (index === digits.length) {
+            rst.push(current);
+            return;
+        }
+
+        for (let c of map[digits[index]]) {
+            dfs(current + c, index + 1);
+        }
+    }
+
+    dfs('', 0);
+    return rst;
+};
+
+console.log(letterCombinations("23"));
