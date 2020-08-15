730f991d218bbd511b5a746b563af98e1675fc96
nanwan03 <wn1842@gmail.com>
2020-08-15 11:16:43 -0700

    commit

---
+++ Algorithm/JavaScript/Search a 2D Matrix II.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    if (!matrix.length || !matrix[0].length) {
+        return false;
+    }
+    let row = 0;
+    let col = matrix[0].length - 1;
+    while (row < matrix.length && col >= 0) {
+        if (matrix[row][col] === target) {
+            return true;
+        } else if (matrix[row][col] < target) {
+            row++;
+        } else {
+            col--;
+        }
+    }
+    return false;
+};
+
+console.log(searchMatrix([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], 5));
