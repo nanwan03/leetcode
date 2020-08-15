c87287188dec198bb9c8ba1af966e4e01731c946
nanwan03 <wn1842@gmail.com>
2020-08-15 10:53:17 -0700

    commit

---
+++ Algorithm/JavaScript/Search Insert Position.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    if (nums.length === 0) {
+        return -1;
+    }
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
+};
+
+console.log(searchInsert([1, 3, 5, 6], 5));
