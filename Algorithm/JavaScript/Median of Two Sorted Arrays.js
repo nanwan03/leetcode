4e5ce6ea6cf8613237d1c3140a1f0ad91da4a07e
nanwan03 <wn1842@gmail.com>
2020-08-16 16:14:11 -0700

    commit

--- Algorithm/JavaScript/Median of Two Sorted Arrays.js
+++ Algorithm/JavaScript/Median of Two Sorted Arrays.js
@@ -6,7 +6,7 @@
 var findMedianSortedArrays = function(nums1, nums2) {
     const length = nums1.length + nums2.length;
 
-    if (length % 2 == 0) {
+    if (length % 2 === 0) {
         return (helper(nums1, 0, nums2, 0, Math.floor(length / 2)) + helper(nums1, 0, nums2, 0, Math.floor(length / 2) + 1)) / 2.0;
     } else {
         return helper(nums1, 0, nums2, 0, Math.floor(length / 2) + 1);
@@ -20,7 +20,7 @@ function helper(nums1, i1, nums2, i2, size) {
     if (i2 >= nums2.length) {
         return nums1[i1 + size - 1];
     }
-    if (size == 1) {
+    if (size === 1) {
         return Math.min(nums1[i1], nums2[i2]);
     }
     const keyA = i1 + Math.floor(size / 2) - 1 < nums1.length ? nums1[i1 + Math.floor(size / 2) - 1] : Number.POSITIVE_INFINITY;
