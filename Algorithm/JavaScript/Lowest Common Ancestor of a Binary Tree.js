a87de9f9d6b93f7f2e4ca0f10bc504f8b613aa7f
nanwan03 <wn1842@gmail.com>
2020-08-17 12:00:30 -0700

    commit

---
+++ Algorithm/JavaScript/Lowest Common Ancestor of a Binary Tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    if (root == null || root === p || root === q) {
+        return root;
+    }
+    const left = lowestCommonAncestor(root.left, p, q);
+    const right = lowestCommonAncestor(root.right, p, q);
+    if (left != null && right != null) {
+        return root;
+    }
+    return left == null ? right : left;
+};
