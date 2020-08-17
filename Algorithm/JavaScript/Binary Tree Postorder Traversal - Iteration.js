4eaa4383de45727383e1b409bdb68871f6da7f7a
nanwan03 <wn1842@gmail.com>
2020-08-17 11:04:42 -0700

    commit

---
+++ Algorithm/JavaScript/Binary Tree Postorder Traversal - Iteration.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    if (!root) {
+        return [];
+    }
+    const rst = [];
+    const stack = [root];
+
+    while (stack.length) {
+        const cur = stack.pop();
+        rst.push(cur.val);
+
+        if (cur.left) {
+            stack.push(cur.left);
+        }
+        if (cur.right) {
+            stack.push(cur.right);
+        }
+    }
+    return rst.reverse();
+};
