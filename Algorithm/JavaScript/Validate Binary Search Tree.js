d27ee9fe6b39ae57b76bf4ca517fa44be6f89be7
nanwan03 <wn1842@gmail.com>
2020-08-17 11:38:02 -0700

    commit

---
+++ Algorithm/JavaScript/Validate Binary Search Tree.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    if (!root) {
+        return true;
+    }
+
+    const helper = (root, min, max) => {
+        if (!root) {
+            return true;
+        }
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+    };
+
+    return helper(root, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY);
+};
