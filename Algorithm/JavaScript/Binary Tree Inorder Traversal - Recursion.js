f5760fff357fe4330bf5275c36b84e0246823fd1
nanwan03 <wn1842@gmail.com>
2020-08-17 10:58:47 -0700

    commit

---
+++ Algorithm/JavaScript/Binary Tree Inorder Traversal - Recursion.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    if (!root) {
+        return [];
+    }
+    return [...inorderTraversal(root.left), root.val, ...inorderTraversal(root.right)];
+};
